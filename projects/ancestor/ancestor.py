3cb29a4c73203a3235e76bf271da3471cf3dd0a8
Jason Sonnichsen <Harliquinn@gmail.com>
2020-02-18 15:23:20 -0500

    Layed out ancestor problem

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -1,3 +1,11 @@
 
 def earliest_ancestor(ancestors, starting_node):
+    #Generate Graph
+    family_tree={}
+    
+    #Create Queue
+    #Add starting_node to Queue
+    #While Queue not empty
+    #Find parents
+    #Return oldest ancestor
     pass
