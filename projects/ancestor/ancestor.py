e6e2e990ee80989860dc04b84ba47bc478df6ea1
Jason Sonnichsen <Harliquinn@gmail.com>
2020-02-18 16:23:29 -0500

    Dealt with not found condition Ancestor complete and tested. Prints removed.

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -12,7 +12,6 @@ class Queue():
         return len(self.queue)
 
 def earliest_ancestor(ancestors, starting_node):
-
     #Generate Graph
     family_tree={}
     for family in ancestors:
@@ -22,11 +21,9 @@ def earliest_ancestor(ancestors, starting_node):
             family_tree[family[1]]={family[0]}
         if not family_tree.get(family[0],None):
             family_tree[family[0]]={}
-    print(family_tree)
-
     #Create Queue
     q=Queue()
-    #Create Results
+    #Create Results set
     family=[]
     #Add starting_node to Queue
     q.enqueue((starting_node,0))
@@ -34,7 +31,6 @@ def earliest_ancestor(ancestors, starting_node):
     #While Queue not empty
     while q.size()>0:
         child=q.dequeue()
-        # print("CHILD",child,"TREE",family_tree[child[0]])
         #Find parents
         for parent in family_tree[child[0]]:
             new_gen=child[1]+1
@@ -43,14 +39,10 @@ def earliest_ancestor(ancestors, starting_node):
                 family[new_gen].append(parent)
             else:
                 family.append([parent])
-
-    print("FAM",family)
     #Return oldest ancestor
-    return min(family[-1])
-
-test_ancestors = [(1, 3), (2, 3), (3, 6), (5, 6), (5, 7), (4, 5), (4, 8), (8, 9), (11, 8), (10, 1)]
-print("ANCIENT1",earliest_ancestor(test_ancestors, 1))
-print("ANCIENT8",earliest_ancestor(test_ancestors, 8))
-print("ANCIENT7",earliest_ancestor(test_ancestors, 7))
-print("ANCIENT9",earliest_ancestor(test_ancestors, 9))
+    methuselah=min(family[-1])
+    if methuselah==starting_node:
+        return -1
+    else:
+        return min(family[-1])
 
