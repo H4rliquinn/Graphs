341ecc3976d65789e07b2f238a5e1d7a5aabfc6f
Jason Sonnichsen <Harliquinn@gmail.com>
2020-02-18 16:14:50 -0500

    Find ancestor complete

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -20,12 +20,37 @@ def earliest_ancestor(ancestors, starting_node):
             family_tree[family[1]].add(family[0])
         else:
             family_tree[family[1]]={family[0]}
-    # print(family_tree)
-    q=Queue()
+        if not family_tree.get(family[0],None):
+            family_tree[family[0]]={}
+    print(family_tree)
+
     #Create Queue
+    q=Queue()
+    #Create Results
+    family=[]
     #Add starting_node to Queue
+    q.enqueue((starting_node,0))
+    family.append([starting_node])
     #While Queue not empty
-    #Find parents
+    while q.size()>0:
+        child=q.dequeue()
+        # print("CHILD",child,"TREE",family_tree[child[0]])
+        #Find parents
+        for parent in family_tree[child[0]]:
+            new_gen=child[1]+1
+            q.enqueue((parent,new_gen))
+            if len(family)==new_gen+1:
+                family[new_gen].append(parent)
+            else:
+                family.append([parent])
+
+    print("FAM",family)
     #Return oldest ancestor
+    return min(family[-1])
+
 test_ancestors = [(1, 3), (2, 3), (3, 6), (5, 6), (5, 7), (4, 5), (4, 8), (8, 9), (11, 8), (10, 1)]
-earliest_ancestor(test_ancestors, 1)
+print("ANCIENT1",earliest_ancestor(test_ancestors, 1))
+print("ANCIENT8",earliest_ancestor(test_ancestors, 8))
+print("ANCIENT7",earliest_ancestor(test_ancestors, 7))
+print("ANCIENT9",earliest_ancestor(test_ancestors, 9))
+
