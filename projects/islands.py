fc20d6dedec9f0e544e7f3dabf919d09c4089d2c
Jason Sonnichsen <Harliquinn@gmail.com>
2020-02-19 12:32:31 -0500

    Set up island counter

---
+++ projects/islands.py
@@ -0,0 +1,11 @@
+# count islands in a binary graph
+
+def island_counter(matrix):
+    
+
+islands = [[0, 1, 0, 1, 0],
+           [1, 1, 0, 1, 1],
+           [0, 0, 1, 0, 0],
+           [1, 0, 1, 0, 0],
+           [1, 1, 0, 0, 0]]
+
