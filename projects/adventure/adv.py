8bd67685c71344c57cf1819c025ae6f6c4e52522
Jason Sonnichsen <Harliquinn@gmail.com>
2020-02-20 16:52:30 -0500

    Went with my gut solution Didn't see why a simple walk wouldn't work and be easier to code than the hinted way. First Pass: TESTS PASSED: 1004 moves, 500 rooms visited

--- projects/adventure/adv.py
+++ projects/adventure/adv.py
@@ -5,6 +5,19 @@ from world import World
 import random
 from ast import literal_eval
 
+class Stack():
+    def __init__(self):
+        self.stack = []
+    def push(self, value):
+        self.stack.append(value)
+    def pop(self):
+        if self.size() > 0:
+            return self.stack.pop()
+        else:
+            return None
+    def size(self):
+        return len(self.stack)
+
 # Load world
 world = World()
 
@@ -13,8 +26,8 @@ world = World()
 # map_file = "maps/test_line.txt"
 # map_file = "maps/test_cross.txt"
 # map_file = "maps/test_loop.txt"
-map_file = "maps/test_loop_fork.txt"
-# map_file = "maps/main_maze.txt"
+# map_file = "maps/test_loop_fork.txt"
+map_file = "maps/main_maze.txt"
 
 # Loads the map into a dictionary
 room_graph=literal_eval(open(map_file, "r").read())
@@ -27,8 +40,58 @@ player = Player(world.starting_room)
 
 # Fill this out with directions to walk
 # traversal_path = ['n', 'n']
-# traversal_path = ['n','n','s','s','s','s','n','n','w','w','e','e','e','e'] Cross
+# traversal_path = ['n','n','s','s','s','s','n','n','w','w','e','e','e','e'] Cross.
 traversal_path = []
+#Create visited
+visited=set()
+# player.current_room.id
+# player.current_room.get_exits()
+# player.travel(direction)
+# get_room_in_direction(direction)
+
+# Create Queue
+s=Stack()
+#Create oposites list
+reverse_dirs = {"n": "s", "s": "n", "e": "w", "w": "e"}
+# add current tuple to queue
+s.push((True,'x'))
+
+# while not empty
+while s.size()>0:
+    # print("Stack",s.stack)
+    #pop current
+    curr_room=s.pop()
+    #get Not_Explored
+    not_explored=curr_room[0]
+    #get dirction
+    last_direction=curr_room[1]
+    #Add to visited
+    visited.add(player.current_room.id)
+    if last_direction !='x':
+        #Queue explored reverse direction
+        if not_explored:
+            s.push((False,reverse_dirs[last_direction]))
+        #move
+        player.travel(last_direction)
+        traversal_path.append(last_direction)
+        print("T_PATH",traversal_path)
+        print("VISITED",visited)
+    if not_explored:
+        #Get exits
+        next_directions=player.current_room.get_exits()
+        # print("NXT DIR",next_directions)
+        #remove previous direction
+        # print("LAST",last_direction,next_directions)
+        if last_direction !='x':
+            next_directions.remove(reverse_dirs[last_direction])
+            #If not previous room Add to stack // And not visited?
+        for dir in next_directions:
+            print()
+            if player.current_room.get_room_in_direction(dir).id not in visited:
+                s.push((True,dir))
+
+
+
 
 
 
